6ed84a52fedfa0647ffdcf02921a4ded4679bd1c
conor <conorlyness@gmail.com>
2022-05-24 19:05:34 +0100

    create home component and set it as default route

--- IGDB/src/app/app-routing.module.ts
+++ IGDB/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //default route
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'search/:game-search',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
